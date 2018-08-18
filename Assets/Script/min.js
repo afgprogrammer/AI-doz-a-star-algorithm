85b3d5bb3917b05ba17ac884e1814e1469b82fce
Mohammad <rto1680@gmail.com>
2018-08-18 11:03:09 +0430

    update child_click function. find path and check win

--- Assets/Script/min.js
+++ Assets/Script/min.js
@@ -103,6 +103,75 @@ for(let i=1;i<=3;i++) {
     }
 }
 
-let child_click = (event) => {
+let child_click = (ev) => {
+    let elem = ev.target.getAttribute('id')
+    click.push(elem)
+    
+    if (click.length <= 3) {
+        // if find match to win
+        if (match.length > 0) {
+            if (click.length >= 1 && click.length <= 2) {
+                for(m of match) {
+                    document.getElementById(m[1]).style.background = '#eee'
+                    document.getElementById(m[2]).style.background = '#eee'
+                }
+            }
+            match.filter((index) => {
+                let ig = index.find((els, index) => {
+                    return els === elem && index === 1
+                })
+                if (ig && typeof ig !== 'undefined') {
+                    match2.push(index)
+                    
+                    if (click.length >= 3)
+                        return;
 
+                    for(m of match2) {
+                        document.getElementById(m[2]).style.background = 'green'
+                    }
+                }
+            })
+        } else {
+            // find match path to win
+            win.filter((index) => {
+                let ig = index.find((els, index) => {
+                    return els === elem && index === 0
+                })
+                if (ig && typeof ig !== 'undefined') {
+                    match.push(index)
+                    for(m of match) {
+                        document.getElementById(m[1]).style.background = 'green'
+                        document.getElementById(m[2]).style.background = 'green'
+                    }
+                }
+            })
+        }
+        ev.target.style.background = 'blue'
+        checkWin(click);
+    }
+}
+
+// check win user
+let checkWin = (win_array) => {
+    return win.find((el) => {
+        if (arraysEqual(win_array, el)) {
+            alert('you win')
+
+            return
+        }
+    })
+}
+
+// check array equal {[user click], [match array]}
+let arraysEqual = (a, b) => {
+    if (a instanceof Array && b instanceof Array) {
+        if (a.length !== b.length) 
+            return false;
+        for(var i=0; i<a.length; i++) 
+            if (!arraysEqual(a[i], b[i]))
+                return false;
+        return true;
+    } else {
+        return a == b;
+    }
 }
